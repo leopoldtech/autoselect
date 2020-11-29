12243da8cfce9bf99e4f24ebf6a99c03d83979d6
leopoldtech <Leopoldwohlgemuth@icloud.com>
2020-11-29 21:00:50 +0100

    Add files via upload

---
+++ Developer/Developer/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Developer
+//
+//  Created by Leopold Wohlgemuth on 28.11.20.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
